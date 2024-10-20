37626cc2f3ed64aef610bae99344d9d622be69ec
subomiakingbade <olasubomi.akingbade@outlook.com>
2024-10-19 22:17:49 -0400

    Board constructor Method to place on the board Method to display the board

---
+++ src/Board.java
@@ -0,0 +1,44 @@
+public class Board {
+    //attributes
+    char[][] board;
+    String word;
+    char direction;
+    int row;
+    int column;
+
+
+
+    //constructor
+    public Board(){
+        board = new char[15][15];
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 15; j++) {
+                board[i][j] = ' ';
+            }
+        }
+    }
+
+
+
+    //methods
+    public void place(String word, char direction, int row, int column){
+        for (int i=0; i< word.length(); i++){
+            if (direction == 'H'){
+                board[row][column + i] = word.charAt(i);
+            } else if (direction == 'V') {
+                board[row + i][column] = word.charAt(i);
+            }
+        }
+    }
+
+    public void display(){
+        for (int i = 0; i < 15; i++){
+            for (int j = 0; j < 15; j++){
+                System.out.printf(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+}
