88364624500cbcb747bd0e4edba4f12befd2312b
Rotimi779 <tomiwaajayi85@yahoo.com>
2024-10-20 21:28:20 -0400

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public ArrayList<Player> players;
@@ -15,26 +16,55 @@ public class Main {
         Player player1 = new Player();
         Player player2 = new Player();
         Player currentPlayer = new Player();
+        for (int i = 0; i < 7; i ++){
+            player1.addTile(tilebag);
+            player2.addTile(tilebag);
+        }
         Board board = new Board();
         boolean play = true;
-        int turn = 0;
+        int turn = 2;
         System.out.println("Welcome to the Game of Scrabble");
 
         while(play){
-            System.out.println("Player 1's turn");
-            currentPlayer =  player1;
-            System.out.println("Pick a tile");
-            boolean validPick = false;
-            while(!(validPick)){
-                currentPlayer.addTile(tilebag);
-                currentPlayer.displayTiles();
-                validPick = true;
+            if (turn == 1){
+                turn = 2;
+                currentPlayer = player2;
             }
-            board.display();
+            else if (turn == 2){
+                turn = 1;
+                currentPlayer = player1;
+            }
+
+            System.out.println("Player " + String.valueOf(turn)  +"'s turn");
+
+            //
+            Scanner tilePick = new Scanner(System.in);
+            System.out.print("Do you want to pick a tile? (yes/no): ");
+            String userInput = tilePick.nextLine().trim().toLowerCase();
+            while(true){
+                if (userInput.equals("yes")) {
+                    System.out.println("Pick a tile");
+                    boolean validPick1 = false;
+                    while(!(validPick1)){
+                        currentPlayer.addTile(tilebag);
+                        currentPlayer.displayTiles();
+                        validPick1 = true;
+                    }
+                } else if (userInput.equals("no")) {
+                    continue;
+                } else {
+                    System.out.println("Invalid input. Please respond with 'yes' or 'no'.");
+                }
+                tilePick.close();
+            }
+
             play = false;
+
         }
+    }
+
 
 
         
-    }
+
 }
