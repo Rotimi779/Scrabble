0248522db1c1c984ce93b802b72d9f5d81f3313a
Rotimi779 <tomiwaajayi85@yahoo.com>
2024-10-20 20:26:32 -0400

    Update

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,19 @@ public class Main {
 
     public static void main(String[] args) {
         TileBag tilebag = new TileBag();
-        Player player1 = new Player();
-        Player player2 = new Player();
         String url = "https://www.mit.edu/~ecprice/wordlist.10000";
         WordDictionary wordDictionary = new WordDictionary(url);
-        System.out.println("Total words loaded: " + wordDictionary.getWordCount());
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Board board = new Board();
+        boolean play = true;
+        int turn = 0;
+        while(play){
+            System.out.println("Welcome to the Game of Scrabble");
+            board.display();
+            play = false;
+        }
 
-        System.out.println("Tiles remaining: " + tilebag.tilesRemaining());
 
     }
 }
