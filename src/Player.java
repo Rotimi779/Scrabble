566c5c99ea21ce93f439a07311e726a1616be573
iheanyi <iheanyinjokuobi@gmail.com>
2024-10-22 12:11:54 -0400

    Rotimi is gay, and that's fine. I also switched some things up!!!

--- src/Player.java
+++ src/Player.java
@@ -1,17 +1,16 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 
 public class Player {
     private int score;
     private List<Tiles> tiles;
+    private static Scanner input;
 
-    public Player(){
+    public Player() {
         tiles = new ArrayList<>();
         this.score = 0;
+        input = new Scanner(System.in);
+
     }
 
     public void addTile(TileBag tileBag){
@@ -39,6 +38,10 @@ public class Player {
         System.out.println("No matching tile found for " + letter + "(" + score + ")");
     }
 
+    public static void close() {
+        input.close();
+    }
+
     public void displayTiles() {
         System.out.println("Player's Tiles:");
         for (Tiles tile : tiles) {
@@ -58,6 +61,85 @@ public class Player {
                     break;
                 }
             }
+        }
+    }
+
+    public void playTurn(){
+        System.out.println("Round " + Game.round);
+        System.out.println("Player " + Game.turn  +"'s turn");
+        this.displayTiles();
+        System.out.print("Type a word or enter P to pass turn: ");
+        String userInput = input.nextLine().trim().toLowerCase();
+        while((!Game.wordDictionary.containsWord(userInput)) ||  (!this.canFormWordFromTiles(userInput))){
+            if (userInput.equals("p")){
+                break;
+            }else if(!this.canFormWordFromTiles(userInput)){
+                System.out.println("The word cannot be formed from the available tiles");
+                System.out.println("Type a new word or enter P to pass turn: ");
+                userInput = input.nextLine().trim().toLowerCase();
+            }
+            else if(!Game.wordDictionary.containsWord(userInput)){
+                System.out.println("The word is not in the dictionary");
+                System.out.println("Type a new word or enter P to pass turn: ");
+                userInput = input.nextLine().trim().toLowerCase();
+            }
+        }
+        if (!userInput.equals("p")){
+            placeWord(userInput);
+        }
+
+        this.updatePlayerScore(userInput);
+        this.displayTiles();
+        Game.board.display();
+
+        if (!userInput.equals("p")){
+            pickTile();
+        }
+    }
+
+    public void placeWord(String word) {
+        boolean placed = false;
+        while(!placed){
+            System.out.println("Where do you want to place the word? ");
+            System.out.println("Row: ");
+            int row = input.nextInt();
+            input.nextLine();
+            System.out.println("Column: ");
+            int column = input.nextInt();
+            input.nextLine();
+            System.out.println("H (Horizontal) or V (Vertical)? ");
+            String direction = input.nextLine().trim().toLowerCase();
+
+            if (direction.equals("horizontal") || direction.equals("h")){
+                placed = Game.board.place(word,'H',row,column);
+
+            }else if(direction.equals("vertical") || direction.equals("v")){
+                placed = Game.board.place(word,'V',row,column);
+            }else{
+                System.out.println("Invalid direction");
+            }
+        }
+    }
+
+    public void pickTile() {
+        //For picking a tile
+        System.out.print("Do you want to pick a tile? (yes/no): ");
+        String tileInput = input.nextLine().trim().toLowerCase();
+        while(true){
+            if (tileInput.equals("yes") || tileInput.equals("y") && this.getNumberOfTiles() < 7) {
+                System.out.println("Pick a tile");
+                boolean validPick1 = false;
+                while(!(validPick1)){
+                    this.addTile(Game.tilebag);
+                    validPick1 = true;
+                }
+                break;
+            } else if (tileInput.equals("no")) {
+                break;
+            } else {
+                System.out.println("Invalid input. Please respond with 'yes' or 'no'.");
+            }
+
         }
     }
 
